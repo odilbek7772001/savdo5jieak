9f7f2fe90f19c3b764c3bd0bf6ea00d9ef0c87a8
odildev2303 <yaropox776@amankro.com>
2024-07-28 15:16:50 +0500

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 // const { i18n } = require('./next-i18next.config')
 
 // // import { i18n } from './next-i18next.config';
- /** @type {import('next').NextConfig} */
+//  /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds:true
-    }
-};
+// const nextConfig = {
+//     eslint: {
+//         ignoreDuringBuilds:true
+//     }
+// };
 
-module.exports = {
-    nextConfig,
-}
+// module.exports = {
+//     nextConfig,
+// }
